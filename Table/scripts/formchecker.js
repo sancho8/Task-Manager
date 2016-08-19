14aa7b0fe89230a20cdc7c1105ef0900b9fc8aeb
sancho8 <98sancho@ukr.net>
2016-08-20 01:16:06 +0300

    fixed input on changeable td's

--- Table/scripts/formchecker.js
+++ Table/scripts/formchecker.js
@@ -18,18 +18,21 @@ $('.redact').mouseleave(function () {
 });
 
 $(".changeable").click(function () {
-    if ($(this).attr('class') == "changeable change-desc") {
+    if ($(this).hasClass("change-desc")) {
+        $(this).parents('td').find("#redacttd").css('display', 'none');
         $(this).parents('td').find(".change-desc-input").css('display', 'inline');
+        $(this).parents('td').find(".change-desc-input").val("sss");
         $(this).parents('td').find(".change-desc-input").focus();
     }
-    if ($(this).attr('class') == "changeable change-data") {
+    if ($(this).hasClass("change-data")) {
         alert("data");
     }
-    if ($(this).attr('class')=="changeable change-prior") {
+    if ($(this).hasClass("change-prior")) {
         alert("prior");
     }
 });
 
-$('.change-desc-input').focusout(function () {
+$('.change-desc-input').focusout(function Asd () {
+    $(this).parents('td').find("#redacttd").css('display', 'inline');
     $(this).css('display', 'none');
 })
